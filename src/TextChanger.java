80abcc118efeda8f7a121a92b15eba1030d71b0a
이동욱 <jojoldu@gmail.com>
2017-04-24 21:59:39 +0900

    Jetbrain balloon 확인창 추가

--- src/TextChanger.java
+++ src/TextChanger.java
@@ -4,7 +4,13 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.ui.awt.RelativePoint;
 
 /**
  * Created by jojoldu@gmail.com on 2017. 4. 24.
@@ -19,7 +25,17 @@ public class TextChanger extends AnAction {
         final Project project = e.getProject();
         String message = getSelectedMessage(e);
 
-        Messages.showMessageDialog(project, message, "텍스트체인저", Messages.getInformationIcon());
+//        Messages.showMessageDialog(project, message, "텍스트체인저", Messages.getInformationIcon());
+
+        StatusBar statusBar = WindowManager.getInstance()
+                .getStatusBar(PlatformDataKeys.PROJECT.getData(e.getDataContext()));
+
+        JBPopupFactory.getInstance()
+                .createHtmlTextBalloonBuilder(message, MessageType.INFO, null)
+                .setFadeoutTime(7500)
+                .createBalloon()
+                .show(RelativePoint.getCenterOf(statusBar.getComponent()),
+                        Balloon.Position.atRight);
     }
 
     private String getSelectedMessage(AnActionEvent e) {
